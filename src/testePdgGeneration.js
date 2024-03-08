e0d0f15afda60656c02d68b5e4f848ca99e9646e
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-03-08 12:15:28 -0400

    add

--- src/testePdgGeneration.js
+++ src/testePdgGeneration.js
@@ -1,26 +1,34 @@
 const pdf = require('html-pdf')
-const fs = require('fs');
-
-const caminhoArquivoHtml = './teste.html';
-const conteudoHtml = fs.readFileSync(caminhoArquivoHtml, 'utf8');
+const ejs = require('ejs')
 
 
-// Configuração para o tamanho A4 (210mm x 297mm)
-const opcoesPdf = {
-    format: 'A4',
-    orientation: 'portrait', // ou 'landscape' para paisagem
-    border: {
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px',
-    },
-};
 
-pdf.create(conteudoHtml, opcoesPdf).toFile("./meupdf.pdf", (err, res) => {
+
+
+
+ejs.renderFile('./src/views/templates/cartela.ejs', {text: 'Gabriel'}, (err, html) => {
     if(err){
-        console.log('Um erro aconteceu', err)
+        console.log('ERRO!', err)
     }else{
-        console.log(res)
+        const opcoesPdf = {
+            format: 'A4',
+            orientation: 'portrait', // ou 'landscape' para paisagem
+            border: {
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px',
+            },
+        };
+        pdf.create(html, opcoesPdf).toFile("./meupdf.pdf", (err, res) => {
+            if(err){
+                console.log('Um erro aconteceu', err)
+            }else{
+                console.log(res)
+            }
+        })
     }
 })
+
+
+
