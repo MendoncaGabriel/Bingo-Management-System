73528faf451e2ce21e57610e87e3e3adac3c48cd
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-04-08 16:52:40 -0400

    add

--- src/routes/pages.router.js
+++ src/routes/pages.router.js
@@ -2,15 +2,23 @@ const express = require('express')
 const router = express.Router()
 const pages = require('../controllers/pages.controller')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 // INTERCEPTA A REQUISIÇÃO E SALVA O TOKEN
 router.use(async (req, res, next) => {
-    if(req.cookies.token){
-        const token = req.cookies.token
-        const user = await jwt.verify(token, process.env.SECRET);
-        req.nameUser = user.user.name
+    try {        
+        if(req.cookies.token){
+            const token = req.cookies.token
+            const SECRET = process.env.SECRET
+            const user = await jwt.verify(token, SECRET);
+            req.nameUser = user.user.name || ''
+        }
+        next()
+    } catch (error) {
+        req.nameUser = ''
+        console.error('Erro ao salvar nameUser na request', error)
+        next()
     }
-    next()
 })
 
 router.get('/nova-cartela', pages.home.create)
