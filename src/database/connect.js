c1ff4bd48817a7b7ff86085a32502ee4ca1566c7
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-04-19 23:55:09 -0300

    add

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const URLCONNECT_DB = process.env.URLCONNECT_DB 
+const URLCONNECT_DB = 'mongodb://mongo:Gam1997SantoBingo@217.196.62.165:27017/bingo'
 module.exports = mongoose.connect(URLCONNECT_DB)
 .then(()=>{
 	console.log('Conectado ao banco de dados!')
