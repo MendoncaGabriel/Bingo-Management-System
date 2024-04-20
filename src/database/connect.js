d07de72bdbc94a2291e0e662b13613904a1849df
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-04-20 12:18:08 -0400

    add

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,8 +1,7 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 const URLCONNECT_DB = process.env.URLCONNECT_DB
 
-module.exports = mongoose.connect(URLCONNECT_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+module.exports = mongoose.connect(URLCONNECT_DB)
 .then(()=>{
 	console.log('Conectado ao banco de dados!')
 	console.log('===> ',URLCONNECT_DB)
